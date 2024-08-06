a26e5fb3bdd0923f444515cbacfebe289344cb81
Jack Harper <jack.harper@stfc.ac.uk>
2024-08-06 14:52:25 +0100

    run safe ruff fixes

--- system_tests/tests/rzbrp100.py
+++ system_tests/tests/rzbrp100.py
@@ -1,12 +1,11 @@
-import unittest
 import itertools
-from parameterized import parameterized
+import unittest
 
+from parameterized import parameterized
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
-
+from utils.testing import get_running_lewis_and_ioc, parameterized_list, skip_if_recsim
 
 DEVICE_NAME = "Rzbrp100"
 DEVICE_PREFIX = "RZBRP100_01"
